6d210fd7a959c3504891c3911caf095e2e3557ef
chetanya-shrimali <chetanyashrimalie5@gmailcom>
2017-10-22 14:59:50 +0530

    added menu models

--- green_apple_website/menu/models.py
+++ green_apple_website/menu/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    category_name = models.CharField(max_length=100)
+
+
+class SubMenu(models.Model):
+    dish_name = models.CharField(max_length=100)
+    price = models.IntegerField(max_length=10)
