7df4c0d599b17dcf1a67f7f7ef6b74ff11e3cc18
chetanya-shrimali <chetanyashrimalie5@gmailcom>
2017-11-04 03:14:03 +0530

    added queries to menu

--- green_apple_website/menu/views.py
+++ green_apple_website/menu/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from .models import SubMenu, Menu
+
 
 def menu(request):
-    return render(request, 'menu/menu.html')
+    all_menu = Menu.objects.all()
+    return render(request, 'menu/menu.html', {'all_menu': all_menu})
+
+
+def sub_menu(request, pk):
+    all_sub_menu = SubMenu.objects.filter(menu=pk)
+    return render(request, 'menu/sub_menu.html',
+                  {'all_sub_menu': all_sub_menu})
