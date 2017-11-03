f2d3e354990cc46271b620f32fdc7b1ac81f0860
chetanya-shrimali <chetanyashrimalie5@gmailcom>
2017-11-04 01:41:50 +0530

    updated models to BCNF

--- green_apple_website/order/admin.py
+++ green_apple_website/order/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Customer, Order, OrderDetail
+
+from .models import Customer, Order, Dish
 
 admin.site.register(Customer)
 admin.site.register(Order)
-admin.site.register(OrderDetail)
+admin.site.register(Dish)
