bfe9781caee64a75649a758515c2849e407a7a84
chetanya-shrimali <chetanyashrimalie5@gmailcom>
2017-11-05 19:11:52 +0530

    added forms

--- green_apple_website/book_a_table/views.py
+++ green_apple_website/book_a_table/views.py
@@ -1,4 +1,5 @@
 from book_a_table.forms import BookForm
+from django.apps import apps
 from django.shortcuts import render
 from django.views import View
 
@@ -21,12 +22,13 @@ class BookFormView(View):
 
 
 class CustomerFormView(View):
-    form_class = BookForm
-    template_name = 'book_a_table/reservations.html'
+    form_class = apps.get_model('order.Customer')
+    template_name = 'book_a_table/customer_registration.html'
 
     def get(self, request):
         form = self.form_class(None)
-        return render(request, 'book_a_table/reservations.html',
+        return render(request,
+                      'book_a_table/../order/templates/order/customer_registration.html',
                       {'form': form})
 
     def post(self):
