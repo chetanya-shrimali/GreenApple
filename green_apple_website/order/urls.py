d7a895c03ea08974285a785a34bee2c0cb704219
Akriti3011 <akriti.bhadoriya96@gmail.com>
2017-11-13 10:47:30 +0530

    order details url added.

--- green_apple_website/order/urls.py
+++ green_apple_website/order/urls.py
@@ -4,5 +4,6 @@ from order import views
 
 app_name = 'order'
 urlpatterns = [
-    url(r'^$', views.order, name='order')
+    url(r'^$', views.order, name='order'),
+    url(r'order_details/$', views.order_details, name='order_details')
 ]
