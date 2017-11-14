7ff99f30a9ef89e2c30d6fe5faba30471e9e8ab6
chetanya-shrimali <chetanyashrimalie5@gmail.com>
2017-11-14 23:52:38 +0530

    updated order model
    Signed-off-by: chetanya-shrimali <chetanyashrimalie5@gmail.com>

--- green_apple_website/order/admin.py
+++ green_apple_website/order/admin.py
@@ -1,20 +1,15 @@
 from django.contrib import admin
 
-from .models import Customer, Order, Dish
-
-
-class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'address', 'phone_number')
+from order.models import Order, Dish
 
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('customer', 'date', 'message')
+    list_display = ('name', 'address', 'phone_number', 'date', 'message')
 
 
-class DishAdmin(admin.ModelAdmin):
-    list_display = ('order', 'name')
+# class DishAdmin(admin.ModelAdmin):
+#     list_display = ('order', 'name')
 
 
-admin.site.register(Customer, CustomerAdmin)
 admin.site.register(Order, OrderAdmin)
-admin.site.register(Dish, DishAdmin)
+admin.site.register(Dish)
--- green_apple_website/order/models.py
+++ green_apple_website/order/models.py
@@ -3,18 +3,20 @@ import datetime
 from django.db import models
 
 
-class Customer(models.Model):
-    name = models.CharField(max_length=50, default='customer name')
-    address = models.CharField(max_length=200)
-    phone_number = models.IntegerField()
-
-
 class Order(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, default='customer name')
+    address = models.CharField(max_length=200, null=True)
+    phone_number = models.IntegerField(null=True)
     date = models.DateField(default=datetime.date.today)
-    message = models.CharField(max_length=500)
+    message = models.CharField(max_length=500, null=True)
+
+    def __str__(self):
+        return self.name + " -> " + str(self.id)
 
 
 class Dish(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    order = models.ManyToManyField(Order)
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
