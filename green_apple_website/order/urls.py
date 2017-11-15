29993bb12cf173775b164aed4ed0e1856380e481
chetanya-shrimali <chetanyashrimalie5@gmail.com>
2017-11-15 20:49:45 +0530

    added order feature
    Signed-off-by: chetanya-shrimali <chetanyashrimalie5@gmail.com>

--- green_apple_website/order/urls.py
+++ green_apple_website/order/urls.py
@@ -5,5 +5,7 @@ from order import views
 app_name = 'order'
 urlpatterns = [
     url(r'^$', views.order, name='order'),
-    url(r'order_details/$', views.order_details, name='order_details')
+    url(r'order_details/$', views.order_details, name='order_details'),
+    url(r'^(?P<pk>[0-9]+)/add_detail/$', views.add_order_details,
+        name='add_detail')
 ]
